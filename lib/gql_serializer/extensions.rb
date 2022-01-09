068212d2e38d4cce252c0f7b45fea675fec4525f
TheDro <andrewsc32@protonmail.com>
2022-01-09 15:27:56 -0500

    Use preload to avoid a query

--- lib/gql_serializer/extensions.rb
+++ lib/gql_serializer/extensions.rb
@@ -23,12 +23,11 @@ module GqlSerializer
     end
 
     def as_gql(query = nil, options = {})
-
       query_hasharray = query ? GqlSerializer.parse_query(query) : []
       include_hasharray = GqlSerializer.query_include(self.class, query_hasharray)
-      record = include_hasharray.empty? ? self : self.class.where(id: self).includes(include_hasharray).first
+      ::ActiveRecord::Associations::Preloader.new.preload(self, include_hasharray)
       options_with_defaults = GqlSerializer.configuration.to_h.merge(options)
-      GqlSerializer.serialize(record, query_hasharray, options_with_defaults)
+      GqlSerializer.serialize(self, query_hasharray, options_with_defaults)
     end
   end
 end
