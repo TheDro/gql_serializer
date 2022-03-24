0b63928765c3b1ff7db99375a93fb043ebd1a944
TheDro <andrewsc32@protonmail.com>
2022-03-24 15:25:36 -0400

    Added test to configuration

--- spec/gql_serializer/configuration_spec.rb
+++ spec/gql_serializer/configuration_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe GqlSerializer::Configuration do
 
     it 'supports none, snake case and camel case' do
       GqlSerializer.configure do |config|
-        config.case= GqlSerializer::Configuration::NONE_CASE
-        config.case= GqlSerializer::Configuration::SNAKE_CASE
-        config.case= GqlSerializer::Configuration::CAMEL_CASE
+        config.case = GqlSerializer::Configuration::NONE_CASE
+        config.case = GqlSerializer::Configuration::SNAKE_CASE
+        config.case = GqlSerializer::Configuration::CAMEL_CASE
       end
 
       expect(GqlSerializer.configuration.case).to eq(:camel)
@@ -18,14 +18,20 @@ RSpec.describe GqlSerializer::Configuration do
 
     it 'raises an error for unsupported cases' do
       GqlSerializer.configure do |config|
-        config.case= :unsupported
+        config.case = :unsupported
       end
 
       raise "error was not raised"
     rescue => e
       expect(e.message).to include("not supported")
     end
+
+    it 'allows preload to be enabled' do
+      GqlSerializer.configure do |config|
+        config.preload = true
+      end
+
+      expect(GqlSerializer.configuration.preload).to eq(true)
+    end
   end
-
-
 end
